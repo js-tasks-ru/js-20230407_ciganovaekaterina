a738fa31fbbb7caaa121151044f4055bdb7d05bc
Ekaterina <ciganovaekaterina@gmail.com>
2023-04-29 19:58:44 +0300

    SortableTable

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -5,20 +5,25 @@ export default class SortableTable {
     this.element = this.render();
   }
 
-  get rowTable() {
+  get subElements() {
+    const result = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+    for (const el of elements) {
+      result[el.dataset.element] = el;
+    }
 
+    return result;
   }
 
   get headerTable() {
-    return this.headerConfig.map((col) => {
+    return this.headerConfig.map((cell) => {
       return `<div
                   class="sortable-table__cell"
-                  data-id=${col.id}
-                  data-sortable=${col.sortable}
-                  data-order="asc"
+                  data-id=${cell.id}
+                  data-sortable=${cell.sortable}
           >
-          <span>${col.title}</span>
-          ${col.sortable ?
+          <span>${cell.title}</span>
+          ${cell.sortable ?
     `<span data-element="arrow" class="sortable-table__sort-arrow">
               <span class="sort-arrow"></span>
             </span>` :
@@ -27,11 +32,30 @@ export default class SortableTable {
     }).join('');
   }
 
+  getCells(item) {
+    return this.headerConfig.map((cell) => {
+      if (cell.id === 'images') {
+        return cell.template(item[cell.id]);
+      }
+      return `<div class="sortable-table__cell">${item[cell.id]}</div>`;
+    }).join('');
+  }
+
+  get bodyTable() {
+    return this.data.map((item) => {
+      return `<a href="/products/${item.id}" class="sortable-table__row">
+          ${this.getCells(item)}
+        </a>`;
+    }).join('');
+  }
+
   get template() {
-    console.log('headerConfig', this.headerConfig);
     return `<div class="sortable-table">
       <div data-element="header" class="sortable-table__header sortable-table__row">
         ${this.headerTable}
+      </div>
+      <div data-element="body" class="sortable-table__body">
+        ${this.bodyTable}
       </div>
     </div>`;
   }
@@ -40,6 +64,33 @@ export default class SortableTable {
     const wrapper = document.createElement("div");
     wrapper.innerHTML = this.template;
     return wrapper.firstElementChild;
+  }
+
+  sort(field, order= 'asc') {
+    const direction = order === 'desc' ? -1 : 1;
+    const sortType = this.headerConfig.find((item) => item.id === field).sortType || 'string';
+
+    function sortFunction(a, b) {
+      if (sortType === 'number') {
+        return a - b;
+      } else {
+        return a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'});
+      }
+    }
+
+    this.data.sort((a, b) => direction * sortFunction(a[field], b[field]));
+    this.subElements.body.innerHTML = this.bodyTable;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
   }
 }
 
